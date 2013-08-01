vmeurisse/paraffin

commit 37ee8bb4e51f6047693775b012b63ba5e293c200

Make port optional when starting server
Port will now be selected automatically when not provided
Port is replaced in remote test url

src/Tests.js
@@ -33,21 +33,16 @@ var Tests = function(config) {
 
 
 Tests.prototype.run = function(cb) {
-	if (this.actions.runServer) {
-		this.startServer();
-	}
-	
 	var actions = [];
 	
+	if (this.actions.runServer) actions.push(this.startServer.bind(this));
 	if (this.actions.runNodeTests) actions.push(this.runTests.bind(this, 'standard'));
 	if (this.actions.prepareCoverage) actions.push(this.prepareCoverage.bind(this));
 	if (this.actions.runNodeCoverage) actions.push(this.runTests.bind(this, 'coverage'));
 	if (this.actions.runRemote) actions.push(this.runRemote.bind(this));
+	if (this.actions.autoStop) actions.push(this.stop.bind(this));
 	
 	require('async').series(actions, function(err) {
-		if (this.actions.autoStop) {
-			this.stop();
-		}
 		if (cb) {
 			// Small delay so that all messages get time to be written to console before returning to caller 
 			setTimeout(function() {
@@ -65,12 +60,12 @@ Tests.prototype.run = function(cb) {
 	}
 };
 
-Tests.prototype.stop = function() {
+Tests.prototype.stop = function(cb) {
 	if (this.actions.coverageReport) {
 		this.coverage.report();
 	}
 	if (this.server) {
-		this.stopServer();
+		this.stopServer(cb);
 	}
 };
 
@@ -80,15 +75,23 @@ Tests.prototype.stop = function() {
  * @method startServer
  * @private
  */
-Tests.prototype.startServer = function() {
+Tests.prototype.startServer = function(cb) {
 	if (!this.server) {
 		var Server = require('./Server');
 		
 		this.server = new Server(this.config.server);
-		this.server.start();
-		if (this.config.url) {
-			console.log('server ready: ' + this.config.url);
-		}
+		this.server.start((function(err, address) {
+			if (this.config.remote && this.config.remote.url) {
+				var url = require('url');
+				var u = url.parse(this.config.remote.url);
+				if (u.port === '0') {
+					delete u.host;
+					u.port = address.port;
+					this.config.remote.url = url.format(u);
+				}
+			}
+			cb();
+		}).bind(this));
 	}
 };
 
@@ -98,10 +101,12 @@ Tests.prototype.startServer = function() {
  * @method stopServer
  * @private
  */
-Tests.prototype.stopServer = function() {
-	this.server.stop();
-	delete this.server;
-	console.log('server stoped');
+Tests.prototype.stopServer = function(cb) {
+	this.server.stop((function() {
+		delete this.server;
+		console.log('server stoped');
+		cb && cb();
+	}).bind(this));
 };
 
 Tests.prototype.runTests = function(type, cb) {

src/Server.js
@@ -11,7 +11,7 @@ var NO_KEEP_ALIVE = {
  * 
  * @param config {Object}
  * @param config.path {String} Path to use as root web folder
- * @param config.port {Number} Port to use for web server
+ * @param [config.port] {Number} Port to use for web server
  * @param [config.coverageDir] {String} Path to coverage directory. Used to write reports from browsers
  */
 var Server = function(config) {
@@ -19,19 +19,22 @@ var Server = function(config) {
 	this.coverage = null;
 };
 
-Server.prototype.start = function() {
-	console.log('starting server');
+Server.prototype.start = function(cb) {
+	console.log('starting server...');
 	var nodeStatic = require('node-static');
 	var staticServer = new nodeStatic.Server(this.config.path, {headers: NO_KEEP_ALIVE});
 	this.server = require('http').createServer(this.handleRequest.bind(this, staticServer));
-	this.server.listen(this.config.port);
+	this.server.listen(this.config.port, (function() {
+		cb(null, this.server.address());
+	}).bind(this));
 };
 
-Server.prototype.stop = function() {
+Server.prototype.stop = function(cb) {
 	if (this.server) {
 		console.log('stoping server');
 		this.server.close();
 		delete this.server;
+		cb();
 	}
 };
 

commit 808ca17a09f275e2b40502e7a92b1123eb080ba4

Allays stop the web server and publish coverage
In version 0.6.3, the server is not stopped when the test are failing.
This was a regression introduced in commit 37ee8bb4e51f6047693775b012b63ba5e293c200

src/Tests.js
@@ -40,16 +40,18 @@ Tests.prototype.run = function(cb) {
 	if (this.actions.prepareCoverage) actions.push(this.prepareCoverage.bind(this));
 	if (this.actions.runNodeCoverage) actions.push(this.runTests.bind(this, 'coverage'));
 	if (this.actions.runRemote) actions.push(this.runRemote.bind(this));
-	if (this.actions.autoStop) actions.push(this.stop.bind(this));
 	
 	require('async').series(actions, function(err) {
-		if (cb) {
-			// Small delay so that all messages get time to be written to console before returning to caller 
-			setTimeout(function() {
-				cb(err);
-			}, 100);
+		if (this.actions.autoStop) {
+			this.stop(function() {
+				if (cb) {
+					// Small delay so that all messages get time to be written to console before returning to caller 
+					setTimeout(function() {
+						cb(err);
+					}, 100);
+				}
+			}.bind(this));
 		}
-		
 	}.bind(this));
 	
 	