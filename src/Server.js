aa983c08942db462d44e99d48454ed3a6ff54fac
Vincent Meurisse <dev@meurisse.org>
2013-08-07 12:03:46 +0200

    New method Server.prototype.addHandler
    It is now possible to register a custom URL handler on the http server

--- src/Server.js
+++ src/Server.js
@@ -1,4 +1,7 @@
 
+var url = require('url');
+var qs = require('querystring');
+
 var HTTP_HEADERS = {
 	'Connection': 'close', // Prevents the browser to keep the connection open. It allows the server to exit quickly.
 	'Cache-Control': 'no-cache' //Make sure that resources are always up-to-date
@@ -12,17 +15,17 @@ var HTTP_HEADERS = {
  * @param config {Object}
  * @param config.path {String} Path to use as root web folder
  * @param [config.port] {Number} Port to use for web server
- * @param [config.coverageDir] {String} Path to coverage directory. Used to write reports from browsers
  */
 var Server = function(config) {
 	this.config = config;
 	this.coverage = null;
+	this.requestHandlers = [];
 };
 
 Server.prototype.start = function(cb) {
 	var nodeStatic = require('node-static');
-	var staticServer = new nodeStatic.Server(this.config.path, {headers: HTTP_HEADERS});
-	this.server = require('http').createServer(this.handleRequest.bind(this, staticServer));
+	this.staticServer = new nodeStatic.Server(this.config.path, {headers: HTTP_HEADERS});
+	this.server = require('http').createServer(this.handleRequest.bind(this));
 	this.server.listen(this.config.port, function() {
 		cb(null, this.server.address());
 	}.bind(this));
@@ -39,32 +42,46 @@ Server.prototype.stop = function(cb) {
 
 Server.prototype.setCoverage = function(coverage) {
 	this.coverage = coverage;
+	this.addHandler('/postResults', function(request, response) {
+		if (request.postData.coverage) {
+			console.log('Adding coverage data for ' + (request.headers['user-agent'] || 'unknown browser'));
+			this.coverage.writeFile(request.postData.coverage);
+			response.writeHead(200, HTTP_HEADERS);
+			response.end('ok\n');
+		} else {
+			response.writeHead(400, HTTP_HEADERS);
+			response.end('Missing coverage data.\n');
+		}
+	}.bind(this));
 };
 
-Server.prototype.handleRequest = function(staticServer, request, response) {
-	if (request.url === '/postResults' && this.coverage && request.method === 'POST') {
-		var qs = require('querystring');
-		var body = '';
+Server.prototype.addHandler = function(url, cb) {
+	this.requestHandlers.push({url: url, callback: cb});
+};
+
+Server.prototype.handleRequest = function(request, response) {
+	var body = '';
+	if (request.method === 'POST') {
 		request.on('data', function (data) {
 			body += data;
 		});
-		request.on('end', function () {
-			var postData = qs.parse(body);
-			if (postData.coverage) {
-				console.log('Adding coverage data for ' + (request.headers['user-agent'] || 'unknown browser'));
-				this.coverage.writeFile(postData.coverage);
-				response.writeHead(200, HTTP_HEADERS);
-				response.end('ok\n');
-			} else {
-				response.writeHead(400, HTTP_HEADERS);
-				response.end('Missing coverage data.\n');
-			}
-		}.bind(this));
-	} else {
-		request.addListener('end', function () {
-			staticServer.serve(request, response);
-		}).resume();
 	}
+	request.on('end', function () {
+		var postData = (request.method === 'POST') ? qs.parse(body) : {};
+		var parsedUrl = url.parse(request.url);
+		request.postData = postData;
+		
+		for (var i = 0; i < this.requestHandlers.length; i++) {
+			if (parsedUrl.pathname === this.requestHandlers[i].url) {
+				request.url = parsedUrl;
+				this.requestHandlers[i].callback(request, response);
+				return;
+			}
+		}
+		
+		this.staticServer.serve(request, response);
+	}.bind(this));
+	request.resume();
 };
 
 module.exports = Server;
